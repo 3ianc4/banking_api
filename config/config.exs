710fe3853b2373886cd6edc9212daa875d898e92
Bianca Guzenski <biancalway@gmail.com>
2021-06-25 13:36:23 -0300

    fix: tidy up config

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,8 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
-use Mix.Config
+import Config
 
 config :banking_api,
   ecto_repos: [BankingApi.Repo]
 
-# Configures the endpoint
 config :banking_api, BankingApiWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "6+apVA9pcXNPD+ATelGodNNXNeMLAeAegkTmhuEF9RvgC/+D0eNxGBa1wKubWpdQ",
@@ -18,14 +10,10 @@ config :banking_api, BankingApiWeb.Endpoint,
   pubsub_server: BankingApi.PubSub,
   live_view: [signing_salt: "xYHpZ7q+"]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
