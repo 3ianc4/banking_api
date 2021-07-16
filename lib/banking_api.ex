efc3f99fb41a51bd7fa0bf22a0b1f64cc3dcce45
Bianca Guzenski <biancalway@gmail.com>
2021-07-16 16:21:41 -0300

    added defdelegates

--- lib/banking_api.ex
+++ lib/banking_api.ex
@@ -6,4 +6,12 @@ defmodule BankingApi do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias BankingApi.Accounts.Account
+  alias BankingApi.Accounts.Transactions
+
+  defdelegate create_account(params), to: Account, as: :execute
+  defdelegate deposit(params), to: Transactions, as: :deposit
+  defdelegate withdraw(params), to: Transactions, as: :withdraw
+  defdelegate transfer(params), to: Transactions, as: :transfer
 end
